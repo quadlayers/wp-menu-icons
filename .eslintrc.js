794751b106e1dad696404bc568d1cbda9cce194f
Martin Meza <martinmezacorreo@gmail.com>
2022-11-23 16:20:31 -0300

    add eslint basic configuration

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    commonjs: true,
+    es6: true,
+    jquery: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
