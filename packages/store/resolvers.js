4ac372a975d075b53ea52f1ddfd8cbcedd3629cc
Martin Meza <martinmezacorreo@gmail.com>
2023-05-26 12:00:14 -0300

    Update resolvers.js

--- packages/store/resolvers.js
+++ packages/store/resolvers.js
@@ -1,15 +1,15 @@
 /**
  * Internal dependencies
  */
-import { fetchRestApiLibraries } from "./helpers"
-import * as actions from "./actions"
+import { fetchRestApiLibraries } from './helpers';
+import * as actions from './actions';
 
+//TODO: test this when the new API is available
 export const getLibraries = async () => {
 	try {
 		const response = await fetchRestApiLibraries();
-		return actions.setLibraries(response)
+		return actions.setLibraries(response);
 	} catch (error) {
-		console.error(error)
+		console.error(error);
 	}
 };
-	
