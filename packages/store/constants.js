c33173cf23417aef4d6251cf094b381e5cbeb02c
Francisco <6652831+franmastromarino@users.noreply.github.com>
2023-05-26 09:00:24 -0300

    Comments

--- packages/store/constants.js
+++ packages/store/constants.js
@@ -1,23 +1,7 @@
 export const STORE_NAME = 'wpmi/store'
 
 export const INITIAL_STATE = {
-	//currentLibrary: null,
+	//currentLibraryName: null,
 	//activeLibraries: [],
 	//libraries = WPMI_LIBRARIES
 }
-
-[
-	{
-		label: 'Test Library',
-		name: 'test-library',
-		type: 'default', // default | custom
-		src: '..../style.css',
-	},
-	{
-		label: 'Test Library',
-		name: 'test-library',
-		type: 'default', // default | custom
-		src: '..../style.css',
-	},
-
-]
--- packages/store/helpers.js
+++ packages/store/helpers.js
@@ -1,8 +1,8 @@
-import wpApiFetch from "@wordpress/api-fetch";
-import { useSelect } from '@wordpress/data'
-import { WPMI_STORE_NAME } from "."
+import wpApiFetch from '@wordpress/api-fetch';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { WPMI_STORE_NAME } from '.';
 
-const { WPMI_REST_ROUTES } = wpmi_backend
+const { WPMI_REST_ROUTES } = wpmi_backend;
 
 /**
  * Handle the response from the apiFetch
@@ -13,9 +13,9 @@ const { WPMI_REST_ROUTES } = wpmi_backend
 export async function apiFetch(args) {
 	return await wpApiFetch(args)
 		.then((response) => {
-			if (!!response.code) {
+			if (response.code) {
 				throw new Error(
-					`${response.code}: ${response?.message || "Unknown"}`
+					`${response.code}: ${response?.message || 'Unknown'}`
 				);
 			}
 			return response;
@@ -33,21 +33,67 @@ export const fetchRestApiLibrary = ({ method, data } = {}) => {
 	});
 };
 
-export const useLibrary = libraryName => {
-    const { library, isResolvingLibrary, hasResolvedLibrary } = useSelect(select=> {
-        const { getLibrary, isResolving, hasFinishedResolution } = select(WPMI_STORE_NAME)
+export const useLibrary = (libraryName) => {
+	const { library, isResolvingLibrary, hasResolvedLibrary } = useSelect(
+		(select) => {
+			const { getLibrary, isResolving, hasFinishedResolution } =
+				select(WPMI_STORE_NAME);
 
-        return {
-            library: getLibrary(libraryName),
-            isResolvingLibrary: isResolving('getLibrary'),
-            hasResolvedLibrary: hasFinishedResolution('getLibrary')
-        }
-    }, [])
+			return {
+				library: getLibrary(libraryName),
+				isResolvingLibrary: isResolving('getLibrary'),
+				hasResolvedLibrary: hasFinishedResolution('getLibrary'),
+			};
+		},
+		[]
+	);
 
-    return {
-        library,
-        isResolvingLibrary,
-        hasResolvedLibrary,
-        hasLibrary: !!(hasResolvedLibrary && library?.length)
-    }
-}
+	return {
+		library,
+		isResolvingLibrary,
+		hasResolvedLibrary,
+		hasLibrary: !!(hasResolvedLibrary && library?.length),
+	};
+};
+
+export const useCurrentLibrary = () => {
+	const {
+		currentLibrary,
+		currentLibraryName,
+		isResolving,
+		hasResolvedCurrentLibraryName,
+	} = useSelect((select) => {
+		const {
+			getCurrentLibraryName,
+			getLibraries,
+			isResolving,
+			hasFinishedResolution,
+		} = select(WPMI_STORE_NAME);
+
+		const currentLibraryName = getCurrentLibraryName();
+		const libraries = getLibraries();
+
+		const currentLibrary = libraries.find(
+			(library) => library.name === currentLibraryName
+		);
+
+		return {
+			currentLibrary,
+			currentLibraryName,
+			isResolving,
+			hasResolvedCurrentLibraryName: hasFinishedResolution(
+				'getCurrentLibraryName'
+			),
+		};
+	}, []);
+
+	const { setCurrentLibraryName } = useDispatch(WPMI_STORE_NAME);
+
+	return {
+		currentLibrary,
+		currentLibraryName,
+		setCurrentLibraryName,
+		isResolving,
+		hasResolvedCurrentLibraryName,
+	};
+};
