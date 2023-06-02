0a52fe0410b597461e3e52f5ee7c723f7aba8500
JulioChaile <65875066+JulioChaile@users.noreply.github.com>
2023-06-02 15:09:16 -0300

    replace WPMI_LIBRARIES with useLibraries

--- packages/backend/app/components/tabs/settings/container/index.js
+++ packages/backend/app/components/tabs/settings/container/index.js
@@ -7,7 +7,7 @@ import { isEqual } from 'lodash';
 import { __ } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
 import { usePrevious } from '@wordpress/compose';
-import { useSettingsEntities } from '@wpmi/store';
+import { useSettingsEntities, useLibraries } from '@wpmi/store';
 /**
  * Internal dependencies
  */
@@ -15,14 +15,14 @@ import { Spinner } from '../../../../../components/spinner';
 import { Container } from '../../../../../components/container';
 // import ImageUploader from "../../../../../components/media-uploader";
 import { OptionLibrary } from '../../../../../components/option-library';
-//TODO: replace with useLibraries
-import { WPMI_LIBRARIES } from '../../../../../helpers';
 import { Modal } from '../../../../../../navmenu/components';
 import NewLibrary from '../../../new-library'
 
 const Settings = () => {
 	const { settings, hasResolvedSettings, saveSettings, setSettings } =
 		useSettingsEntities();
+
+	const { libraries, hasResolvedLibraries } = useLibraries()
 
 	const [show, setShow] = useState(false)
 
@@ -50,19 +50,18 @@ const Settings = () => {
 		});
 	};
 
-	const submitSettings = async (e) => {
+	const submitSettings = e => {
 		e.preventDefault();
 		setIsLoading(true);
-		const result = await saveSettings(settings);
-		console.log('result: ', result);
-		console.log('settings: ', settings);
 
-		setIsLoading(false);
+		saveSettings(settings).then(() => setIsLoading(false))
 	};
+
+	const filterLibraries = type => libraries.filter(library => library.type === type)
 
 	const onClose = () => setShow(false)
 
-	if (!hasResolvedSettings) {
+	if (!hasResolvedSettings && !hasResolvedLibraries) {
 		return <Spinner />;
 	}
 
@@ -71,9 +70,24 @@ const Settings = () => {
 			{__('Enable Libraries', 'wp-menu-icons')}
 			<form onSubmit={submitSettings}>
 				{__('Default', 'wp-menu-icons')}
-				{Object.values(WPMI_LIBRARIES || []).map((library) => {
-					if (library.type === 'default') {
-						return (
+				{filterLibraries('default').map((library) => 
+					<OptionLibrary
+						key={library.name}
+						label={library.label}
+						checked={
+							!!settings.active_libraries?.includes(
+								library.name
+							)
+						}
+						onChange={() =>
+							togleActiveLibraries(library.name)
+						}
+					/>
+				)}
+				{filterLibraries('uploaded').length !== 0 && 
+					<>
+						{__('Customize', 'wp-menu-icons')}
+						{filterLibraries('uploaded').map((library) => 
 							<OptionLibrary
 								key={library.name}
 								label={library.label}
@@ -86,32 +100,9 @@ const Settings = () => {
 									togleActiveLibraries(library.name)
 								}
 							/>
-						);
-					}
-				})}
-				{WPMI_LIBRARIES && Object.keys((WPMI_LIBRARIES || [])).length !== 0 && (
-					<>
-						{__('Customize', 'wp-menu-icons')}
-						{Object.values(WPMI_LIBRARIES).map((library) => {
-							if (library.type === 'uploaded') {
-								return (
-									<OptionLibrary
-										key={library.name}
-										label={library.label}
-										checked={
-											!!settings.active_libraries?.includes(
-												library.name
-											)
-										}
-										onChange={() =>
-											togleActiveLibraries(library.name)
-										}
-									/>
-								);
-							}
-						})}
+						)}
 					</>
-				)}
+				}
 
 				<span className="spinner"></span>
 				<button
