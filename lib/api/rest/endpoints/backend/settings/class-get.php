87bba08a240d7a8b3517c8f7524cbdc95d2a7c57
KanNeimann <kan_a_n@hotmail.com>
2023-06-05 09:59:11 -0300

    Handle error cases y success in endpoints

--- lib/api/rest/endpoints/backend/settings/class-get.php
+++ lib/api/rest/endpoints/backend/settings/class-get.php
@@ -9,11 +9,19 @@ class Get extends Base {
 	protected static $route_path = 'settings';
 
 	public function callback( \WP_REST_Request $request ) {
-		$setting_model = new Setting();
+		try {
+			$setting_model = new Setting();
 
-		$settings = $setting_model->get();
+			$settings = $setting_model->get();
 
-		return $this->handle_response( $settings );
+			return $this->handle_response( $settings );
+		} catch ( \Throwable $error ) {
+			$response = array(
+				'code'    => $error->getCode(),
+				'message' => $error->getMessage(),
+			);
+			return $this->handle_response( $response );
+		}
 	}
 
 	public static function get_rest_method() {
