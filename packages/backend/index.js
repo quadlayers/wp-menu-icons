4e46069794ecde22eaa337d593f3495daee9ff0f
Martin Meza <martinmezacorreo@gmail.com>
2023-06-22 11:48:27 -0300

    add exports

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -5,6 +5,9 @@
 import { render } from '@wordpress/element';
 import { App } from './app';
 import { onDocumentLoaded } from './helpers';
+export * from './app';
+export * from './components';
+export * from './helpers';
 
 onDocumentLoaded(() => {
 	const target = document.getElementById('wpbody-content');
